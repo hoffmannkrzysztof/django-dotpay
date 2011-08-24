dc8cd9cff9e02bedcd064bbd8c310c504293e263
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2011-08-24 19:32:27 +0200

    version change

--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import setup, find_packages
     
 setup(
     name = 'django-dotpay',
-    version = '0.2',
+    version = '0.2.1',
     author = 'Krzysztof Hoffmann',
     author_email = 'krzysiekpl@gmail.com',
     license='BSD',
     url = 'https://github.com/krzysiekpl/django-dotpay/',
     packages = ['dotpay','dotpay.sms','dotpay.payment'],
-    zip_safe = True,
-    install_requires = ['django',],
     )

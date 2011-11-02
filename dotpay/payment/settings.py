3379ac3f5f41cb4a8a742b34d1d57c398183da28
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2011-11-02 12:47:28 +0100

    DOTURLC remove

--- dotpay/payment/settings.py
+++ dotpay/payment/settings.py
@@ -1,8 +1,6 @@
 # -*- encoding: utf-8 -*-
 from django.conf import settings
-from django.contrib.sites.models import Site
-from urlparse import urljoin
-from django.core.urlresolvers import reverse
+
 
 try:
     DOTPIN = getattr(settings, 'DOTPIN')
@@ -13,15 +11,5 @@ try:
     DOTURL  = getattr(settings, 'DOTURL')
 except AttributeError:
     raise BaseException("DOTURL wymagany")
-
-try:
-    site = Site.objects.get_current()
-    domain = site.domain
-    if domain.startswith("http://"):
-        DOTURLC = urljoin(domain,reverse('dotpay_receiver'))
-    else:
-        DOTURLC = urljoin("http://"+domain,reverse('dotpay_receiver'))
-except AttributeError:
-    raise BaseException("DOTURLC wymagany. Błąd SITE.")
 
 DOTTXTGUZIK  = getattr(settings, 'DOTTXTGUZIK', 'Powrót do sklepu')

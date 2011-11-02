8fb8f3b05d11868f5fc52222e6ccb78ccfc3e34f
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2011-11-02 12:49:04 +0100

    rev change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
     
 setup(
     name = 'django-dotpay',
-    version = '0.2.4',
+    version = '0.2.5',
     author = 'Krzysztof Hoffmann',
     author_email = 'krzysiekpl@gmail.com',
     license='BSD',

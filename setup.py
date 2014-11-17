8143bb46434fbf8ac962f221722d45ea13860eba
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2014-11-17 10:47:06 +0100

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
     
 setup(
     name = 'django-dotpay',
-    version = '0.2.8',
+    version = '0.2.9',
     author = 'Krzysztof Hoffmann',
     author_email = 'krzysiekpl@gmail.com',
     license='BSD',
